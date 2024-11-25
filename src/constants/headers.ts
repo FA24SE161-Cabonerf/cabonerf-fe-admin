4353b3c583794d46897db66a4c458e4de3372ba1
00stickman007 <vinhbqse160534@fpt.edu.vn>
2024-11-25 10:33:49 +0700

    add authorization header

--- src/constants/headers.ts
+++ src/constants/headers.ts
@@ -8,3 +8,4 @@ export const headers = {
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUyMTE4NmI3ZjRhNmM3YzBhZjllNWQ2YzZkY2NmNmRhXG4iLCJpYXQiOjE3Mjk1NzU3Njl9.RblWbflm4C3ZMW7kp4Xw3WNznd2OX38fdK-6FVSD768",
   authorization: `Bearer ${authorization}`,
 };
+  
